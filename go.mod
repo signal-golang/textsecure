f5f7e29523c82b6b5e836d3e7b3471a3d80cf417
Thorsten Ploß <thorsten.ploss@wtnet.de>
2020-10-25 02:20:10 +0200

    avatar decryption with aes_gcm_siv
    Not correct by now. The tests in server_test.go still fail.

--- go.mod
+++ go.mod
@@ -7,6 +7,11 @@ require (
 	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/websocket v1.4.2
+	github.com/jacobsa/crypto v0.0.0-20190317225127-9f44e2d11115
+	github.com/jacobsa/oglematchers v0.0.0-20150720000706-141901ea67cd // indirect
+	github.com/jacobsa/oglemock v0.0.0-20150831005832-e94d794d06ff // indirect
+	github.com/jacobsa/ogletest v0.0.0-20170503003838-80d50a735a11 // indirect
+	github.com/jacobsa/reqtrace v0.0.0-20150505043853-245c9e0234cb // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo v1.7.0 // indirect
--- server.go
+++ server.go
@@ -5,8 +5,6 @@ package textsecure
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
@@ -21,7 +19,11 @@ import (
 	"github.com/signal-golang/textsecure/protobuf"
 
 	log "github.com/sirupsen/logrus"
+
+	aesgcmsiv "github.com/jacobsa/crypto/siv"
 )
+
+const NONCE_LEN = 12
 
 var (
 	createAccountPath      = "/v1/accounts/%s/code/%s?client=%s"
@@ -383,26 +385,36 @@ func GetAvatar(avatarUrl string) (io.ReadCloser, error) {
 }
 
 func decryptAvatar(avatar []byte, identityKey []byte) []byte {
-	block, err := aes.NewCipher(identityKey[:32])
-	log.Debugln("-0", avatar[:12])
-	if err != nil {
-		log.Debugln("0", err)
-
-		return nil
-	}
-	// nonce := []byte{}
-	aesgcm, err := cipher.NewGCM(block)
-	log.Debugln("-0", aesgcm.NonceSize())
-
-	if err != nil {
-		log.Debugln("1", err)
-	}
-	b, err := aesgcm.Open(nil, avatar[:12], avatar, nil)
+	avatar_len := len(avatar) - NONCE_LEN
+	nonce := avatar[avatar_len:]
+	encrypted_avatar := avatar[:avatar_len]
+	decryptedAvatar, err := decrypt(identityKey[:32], encrypted_avatar, nonce)
 	if err != nil {
 		log.Debugln("3", err)
+		return nil
 	}
-	return b
+	return decryptedAvatar
 }
+
+func decrypt(key, data, nonce []byte) ([]byte, error) {
+	associated := [][]byte{nonce}
+	decrypted, err := aesgcmsiv.Decrypt(key, data, associated)
+	if err != nil {
+		return nil, err
+	}
+	return decrypted, nil
+}
+
+func encrypt(key, data, nonce []byte) ([]byte, error) {
+	associated := [][]byte{nonce}
+	dest := []byte{}
+	encrypted, err := aesgcmsiv.Encrypt(dest, key, data, associated)
+	if err != nil {
+		return nil, err
+	}
+	return encrypted, nil
+}
+
 func generateNonce(avatar []byte, length int) []byte {
 	var offset int
 	offset = 0
